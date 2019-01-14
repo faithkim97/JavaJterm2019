a53556bfbb540c8438f2adc754b73d438d45e2ce
Faith Kim <fkim@smith.edu>
2019-01-13 20:32:45 -0500

    adding file1 file2

---
+++ Methods/File1.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class File1 {
+
+  public static void printHelloWorld() {
+    System.out.println("Hello World!");
+  }
+
+
+}
---
+++ Methods/File2.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class File2 {
+
+  public static void main(String[] args) {
+    File1.printHelloWorld();
+  }
+
+}
